f50bb2761417d7a04f160683376dcfb6602bd6cd
April <45841039+xcwangbcp@users.noreply.github.com>
2022-04-02 16:22:01 +0800

    update for the apple start part

--- Dexterity.m
+++ Dexterity.m
@@ -7,10 +7,11 @@ filename_raw_apple           = uigetfile('*.csv','Pick an apple tracking csv fil
 [raw_apple,Txt_apple,~]      = xlsread([pathname,filename_raw_apple]);
 % filename_raw_hand  =  'D:\Code\Data\Apple\2-trimmed.csv';
 % raw_apple = table2array(readtable(filename_raw_apple));
-M.slot     = findcolum(Txt_apple,'SlotTop_x','SlotBottom_x');
-Edge(:,1)  = mean(raw_apple(:,M.slot(1)));
-Edge(:,2)  = mean(raw_apple(:,M.slot(2)));
-Edge       = mean(Edge);
+M.slot     = findcolum(Txt_apple,'SlotTop_x','SlotBottom_x','SlotTop_y','SlotBottom_y');
+Edge_x     = (mean(raw_apple(:,M.slot(1)))+mean(raw_apple(:,M.slot(2))))/2;
+Edge_y_top = mean(raw_apple(:,M.slot(3)));
+Edge_y_bot = mean(raw_apple(:,M.slot(4)));
+
 M.tip2     = findcolum(Txt_hand,'tip2_x','tip2_y','tip2_p'); % indexfinger tip 
 index_tip  = raw_hand(:,M.tip2(1));
 index_tip_y= raw_hand(:,M.tip2(2));
@@ -34,9 +35,9 @@ apple_p   = raw_apple(:,M.apple(3));
 
 threshold_apple = 0.2;
 % apple_y(apple_p<threshold_apple)=nan;
-apple(apple_p<threshold_apple) = nan;
-apple(apple<Edge )             = nan;
-apple(apple>340)               = nan; % 350 is the board of the glass
+apple(apple_p<threshold_apple)   = nan;
+apple(apple<Edge_x )             = nan;
+apple(apple>340)                 = nan; % 350 is the board of the glass
 apple     = movmean(apple,5);% average in 5 frames
 
 % arti_locs   = find(abs(diff_apple)>15);
@@ -99,7 +100,7 @@ point_start2 = locs_ss;
 % plot(apple(point_start),point_start,'marker','*','color','blue')
 hold on 
 plot(apple(point_start2),point_start2,'marker','o','color','red')
-
+title('filename_raw_hand')
 point_start= point_start2;
 fwd_count = 0; want_count = 0; success_count = 0;
 fwd_time  =[]; back_time  = [];
@@ -123,7 +124,7 @@ for i=1:apple_num
 
     for j=time_window(1):time_window(end)%apple_disappear_time(i)                 % 从第i个苹果出来，到第i个苹果消失的time window
         
-         if index_tip(j-2,1)<index_tip(j-1,1)&&index_tip(j-1,1)<index_tip(j,1)&&index_tip(j,1)<= Edge && index_tip(j+1,1)>=Edge...
+         if index_tip(j-2,1)<index_tip(j-1,1)&&index_tip(j-1,1)<index_tip(j,1)&&index_tip(j,1)<= Edge_x && index_tip(j+1,1)>=Edge_x...
                  &&index_tip(j+2,1)>index_tip(j+1,1)&&~isnan(apple(j))  
              % 如果食指在该时间窗内伸入挡板，计为一次伸??,并且苹果??
 %              if index_tip_y(j)>130&&index_tip_y(j)<195    % if apple is no the pole,
@@ -134,8 +135,8 @@ for i=1:apple_num
 %                 want_count = want_count+1;
                 k = j;                                        %进挡板后至少5帧，去判断出手的时间
                 while 1
-                    if index_tip(k+1,1)<index_tip(k,1)&&index_tip(k,1)<=Edge...
-                       &&index_tip(k-1,1)>=Edge%&&index_tip(k-1,1)<index_tip(k-2,1) index_tip(k+2,1)<index_tip(k+1,1)&&
+                    if index_tip(k+1,1)<index_tip(k,1)&&index_tip(k,1)<=Edge_x...
+                       &&index_tip(k-1,1)>=Edge_x%&&index_tip(k-1,1)<index_tip(k-2,1) index_tip(k+2,1)<index_tip(k+1,1)&&
                         success_count = success_count+1;% 如果条件设成5点的，会损失某些??
                         back_time     = [back_time,k];
                         break
@@ -152,7 +153,8 @@ for i=1:apple_num
     end
 end
 % grab_time = [back_time; fwd_time]';
-% delete the activity when 
+% delete the activity when the hand is out of the ROI which defined by the
+% slit
 j=1;
 for  i=1:length(back_time)
    
@@ -163,46 +165,46 @@ for  i=1:length(back_time)
     end 
 end 
 back_time(locs)=nan;fwd_time(locs)=nan;
-grab_time = [back_time(~isnan(back_time)); fwd_time(~isnan(fwd_time))]';
-locs      = grab_time>12;% 132-5-31 低于12帧的是没有获取到苹果的数据，其他猴子待验??
-grab_time = grab_time(locs)/60;
-
-for i = 9:nframes(end)-1
-    if index_tip(i-2,1) < Edge &&index_tip(i-1,1) < Edge && index_tip(i,1) >= Edge&&index_tip(i+1,1)>Edge&&index_tip(i+2,1)>Edge
-        if apple(i)>Edge&&apple(i)<230  % 20 pixles
-        time_fwd =[time_fwd,i];
-        trial_count = trial_count+1;
-        hold on
-        p=p+1;
-        text(Edge-10,i,[num2str(p),'\rightarrow'],'Color','red','FontSize',15)
-        j=i;
-            while 1   
-            j=j+1
-                if j>=nframe(end)||j>=i+60*60;
-                   break
-                end
-                if index_tip(j-2,1) > Edge&&index_tip(j-1,1) > Edge && index_tip(j,1) <= Edge&&index_tip(j+1,1)< Edge&&index_tip(j+2,1)<Edge
-                    time_back = [time_back,j];
-%                 if j ==time_back(q)
-%                    j=nan;
+grab_time = [ fwd_time(~isnan(fwd_time));back_time(~isnan(back_time))]';
+% locs      = grab_time>12;% 132-5-31 低于12帧的是没有获取到苹果的数据，其他猴子待验??
+% grab_time = grab_time(locs)/60;
+% 
+% for i = 9:nframes(end)-1
+%     if index_tip(i-2,1) < Edge &&index_tip(i-1,1) < Edge && index_tip(i,1) >= Edge&&index_tip(i+1,1)>Edge&&index_tip(i+2,1)>Edge
+%         if apple(i)>Edge&&apple(i)<230  % 20 pixles
+%         time_fwd =[time_fwd,i];
+%         trial_count = trial_count+1;
+%         hold on
+%         p=p+1;
+%         text(Edge-10,i,[num2str(p),'\rightarrow'],'Color','red','FontSize',15)
+%         j=i;
+%             while 1   
+%             j=j+1
+%                 if j>=nframe(end)||j>=i+60*60;
+%                    break
 %                 end
-                    q=q+1;
-                    text(Edge+10,j,[num2str(q),'\leftarrow'],'Color','green','FontSize',15)
-                    break
-                end   
-            end
-        end
-    end
-    
-%     if TIP2(i,1) >= Edge && TIP2(i+1,1) <= Edge%&&TIP2(i+10,1)<Edge% && sum(isnan(apple_start(i-8:i))) > 0
-%         time_back = [time_back,i];
-%     end     
-end
-
-
-
-
-
+%                 if index_tip(j-2,1) > Edge&&index_tip(j-1,1) > Edge && index_tip(j,1) <= Edge&&index_tip(j+1,1)< Edge&&index_tip(j+2,1)<Edge
+%                     time_back = [time_back,j];
+% %                 if j ==time_back(q)
+% %                    j=nan;
+% %                 end
+%                     q=q+1;
+%                     text(Edge+10,j,[num2str(q),'\leftarrow'],'Color','green','FontSize',15)
+%                     break
+%                 end   
+%             end
+%         end
+%     end
+%     
+% %     if TIP2(i,1) >= Edge && TIP2(i+1,1) <= Edge%&&TIP2(i+10,1)<Edge% && sum(isnan(apple_start(i-8:i))) > 0
+% %         time_back = [time_back,i];
+% %     end     
+% end
+% 
+% 
+% 
+% 
+% 
 
 
 
@@ -212,60 +214,60 @@ end
 %         
 %     end
 % end
-hold on
-
-% distance  = abs(diff(apple));
-% apple_out(:,1) = apple(distance>30);
-% apple_out(:,2) = find(distance>30)+2;
-% % apple_out(:,1) = apple(apple_out(:,2));
-% for i=1:length(apple_out(:,1))
-%     if apple_out(i,1)>250
-%         apple_out(i,1)=NaN;
-%     end
-% end
-% plot(apple_out(:,1),apple_out(:,2),'marker','*','color','blue')
-
-% pole
-M.pole   = findcolum(Txt_apple,'Pole_x','Pole_y','Pole_p');
-pole_x   = raw_apple(:,M.pole(1));
-pole_p   = raw_apple(:,M.pole(3));
-% nframe   = 1:size(raw_apple,1);
-% nframe = 60*60;
-index_tip = index_tip(1:nframe);
-apple     = apple(1:nframe);
-pole_x    = pole_x(1:nframe);
-pole_x(pole_x<mode(pole_x) )     = nan;
-pole_x(apple_p<threshold)= nan;
-hold on
-plot(pole_x,nframes,'c*')
-
-
-
- 
-% plot(pole_x,nframes,'color','blue')
-touch    = 0; 
-trial_count = 0;
-
-p=0;q=0;
-
-
-t=(time_back-time_fwd);
-
-[location] = inthepicture(raw,M);
-index_x  = [raw(location,M.base(1)),raw(location,M.MCP2(1)),raw(location,M.PIP2(1)),...
-           raw(location,M.DIP2(1)),raw(location,M.tip2(1))];
-index_y  = [raw(location,M.base(2)),raw(location,M.MCP2(2)),raw(location,M.PIP2(2)),...
-           raw(location,M.DIP2(2)),raw(location,M.tip2(2))];       
-        
-        
-thumb_x  = [raw(location,M.base(1)),raw(location,M.MCP1(1)),raw(location,M.PIP1(1)),raw(location,M.tip1(1))];
-thumb_y  = [raw(location,M.base(2)),raw(location,M.MCP1(2)),raw(location,M.PIP1(2)),raw(location,M.tip1(2))];
-%hand_y   = [raw(:,M.base(2)),raw(:,M.MCP1(2)),raw(:,M.MIP1(2)),raw(:,M.tip1(2))...
-            %raw(:,M.MCP2(2)),raw(:,M.PIP2(2)),raw(:,M.DIP2(2)),raw(:,M.tip2(2))];
-plot(index_x,index_y,'marker','+');
-set(gca,'YDir','reverse')
-hold on 
-plot(thumb_x(:,1),thumb_y(:,1),'marker','diamond')
+% hold on
+% 
+% % distance  = abs(diff(apple));
+% % apple_out(:,1) = apple(distance>30);
+% % apple_out(:,2) = find(distance>30)+2;
+% % % apple_out(:,1) = apple(apple_out(:,2));
+% % for i=1:length(apple_out(:,1))
+% %     if apple_out(i,1)>250
+% %         apple_out(i,1)=NaN;
+% %     end
+% % end
+% % plot(apple_out(:,1),apple_out(:,2),'marker','*','color','blue')
+% 
+% % pole
+% M.pole   = findcolum(Txt_apple,'Pole_x','Pole_y','Pole_p');
+% pole_x   = raw_apple(:,M.pole(1));
+% pole_p   = raw_apple(:,M.pole(3));
+% % nframe   = 1:size(raw_apple,1);
+% % nframe = 60*60;
+% index_tip = index_tip(1:nframe);
+% apple     = apple(1:nframe);
+% pole_x    = pole_x(1:nframe);
+% pole_x(pole_x<mode(pole_x) )     = nan;
+% pole_x(apple_p<threshold)= nan;
+% hold on
+% plot(pole_x,nframes,'c*')
+% 
+% 
+% 
+%  
+% % plot(pole_x,nframes,'color','blue')
+% touch    = 0; 
+% trial_count = 0;
+% 
+% p=0;q=0;
+% 
+% 
+% t=(time_back-time_fwd);
+% 
+% [location] = inthepicture(raw,M);
+% index_x  = [raw(location,M.base(1)),raw(location,M.MCP2(1)),raw(location,M.PIP2(1)),...
+%            raw(location,M.DIP2(1)),raw(location,M.tip2(1))];
+% index_y  = [raw(location,M.base(2)),raw(location,M.MCP2(2)),raw(location,M.PIP2(2)),...
+%            raw(location,M.DIP2(2)),raw(location,M.tip2(2))];       
+%         
+%         
+% thumb_x  = [raw(location,M.base(1)),raw(location,M.MCP1(1)),raw(location,M.PIP1(1)),raw(location,M.tip1(1))];
+% thumb_y  = [raw(location,M.base(2)),raw(location,M.MCP1(2)),raw(location,M.PIP1(2)),raw(location,M.tip1(2))];
+% %hand_y   = [raw(:,M.base(2)),raw(:,M.MCP1(2)),raw(:,M.MIP1(2)),raw(:,M.tip1(2))...
+%             %raw(:,M.MCP2(2)),raw(:,M.PIP2(2)),raw(:,M.DIP2(2)),raw(:,M.tip2(2))];
+% plot(index_x,index_y,'marker','+');
+% set(gca,'YDir','reverse')
+% hold on 
+% plot(thumb_x(:,1),thumb_y(:,1),'marker','diamond')
 
 function [location_hand] = inthepicture(raw,M)  % find the lines with all the likelyhood more than 1
     likelyhood = [raw(:,M.base(3)),raw(:,M.MCP1(3)),raw(:,M.PIP1(3)),raw(:,M.tip1(3))...
@@ -278,7 +280,7 @@ end
 
 
 % find the relative columns for each traking piont
-function [column]  = findcolum(Txt,A,B,C)
+function [column]  = findcolum(Txt,A,B,C,D)
         switch nargin  
             case 2
                  Aa        = strcmp(Txt(1:end),A);
@@ -288,12 +290,21 @@ function [column]  = findcolum(Txt,A,B,C)
                 Ba        = strcmp(Txt(1:end),B);
                 column(1) = find(Aa==1);
                 column(2) = find(Ba==1);
-            otherwise
+            case 4
                 Aa        = strcmp(Txt(1:end),A);       
                 Ba        = strcmp(Txt(1:end),B);
                 Ca        = strcmp(Txt(1:end),C);
                 column(1) = find(Aa==1);
                 column(2) = find(Ba==1);
                 column(3) = find(Ca==1);
+            otherwise
+                Aa        = strcmp(Txt(1:end),A);       
+                Ba        = strcmp(Txt(1:end),B);
+                Ca        = strcmp(Txt(1:end),C);
+                Da        = strcmp(Txt(1:end),D);
+                column(1) = find(Aa==1);
+                column(2) = find(Ba==1);
+                column(3) = find(Ca==1);
+                column(4) = find(Da==1);
         end          
 end
